db2f7e51d434e644ea4cf292d3b8158ac0ac6471
manhtai <vo@manhtai.com>
2024-11-25 22:32:42 +0700

    Update

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-monolith https://hobao.iwedding.info/ -o index.html
+monolith -M https://hobao.iwedding.info/ -o index.html
+sd 'https:\\/\\/cdn.biihappy.com\\/ziiweb\\/wedding-musics\\/IDo-911.mp3' 'https:\\/\\/huyentrang.manhtai.com\\/songs.mp3' index.html
+cat index.html | grep songs.mp3
 git commit -am "Update"
 git push
